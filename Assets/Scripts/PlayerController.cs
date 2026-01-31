617dbd22a1245a3c484c3341a84d81b7b1204265
Jun Earl Solomon <junearlsolomon14@gmail.com>
2026-01-31 14:39:21 -0800

    Bring in free jungle tileset and set up basic movement controls.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput.normalized * moveSpeed;
+    }
+}
