cbf5c026aead36554464fd550d1713289d3fa947
Jun Earl Solomon <junearlsolomon14@gmail.com>
2026-01-31 15:19:51 -0800

    Implement limited vision cone.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Vector2 lastMoveInput;
+    private Transform spotLight;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spotLight = transform.Find("Light 2D");
+        lastMoveInput = Vector2.right;
     }
 
     void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if (moveInput.magnitude > 0.1f)
+        {
+            lastMoveInput = moveInput.normalized;
+            
+            float angle = Mathf.Atan2(lastMoveInput.y, lastMoveInput.x) * Mathf.Rad2Deg;
+            spotLight.rotation = Quaternion.Euler(0f, 0f, angle - 90);
+        }
     }
 
     void FixedUpdate()
